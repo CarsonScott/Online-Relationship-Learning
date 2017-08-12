ae7a7d0b88c9669897cffc6212e7329794605b77
Carson Scott <carsonjscott14@gmail.com>
2017-08-12 02:25:50 -0400

    Added Relation

--- src/Function.py
+++ src/Function.py
@@ -2,3 +2,6 @@ e = 2.718281828459
 
 def sigmoid(x, a, b, c):
 	return a / (1+pow(e, -c * (x-b)))
+
+def gaussian(x, a, b, c):
+	return a * pow(e, -pow(x-b, 2) / pow(2*c, 2))
---
+++ src/Relation.py
@@ -0,0 +1,43 @@
+from Function import gaussian
+from time import clock
+
+class Relation:
+
+	def __init__(self, f, l):
+		self.delay  = 0.5
+		self.range  = 0.5
+		self.weight = 1.0
+
+		self.f = f
+		self.l = l
+
+		self.ff = None
+		self.lf = None
+
+	def reset(self):
+		self.ff = None
+		self.lf = None
+
+	def valid(self):
+		f = self.ff
+		l = self.lf
+		return None not in (f,l)
+	
+	def fitness(self, x):
+		a = self.weight
+		b = self.delay
+		c = self.range
+
+		return gaussian(x, a, b, c)
+
+	def update(self, fs, ls, t):
+		if fs != 0: self.ff = t
+		if ls != 0: self.lf = t
+		y = None
+
+		if self.valid():
+			x = self.lf-self.ff
+			y = self.fitness(x)		
+			self.reset()
+
+		return y
