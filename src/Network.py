7f400a20c5587f78fc6e9a2ff43d4890f4a1a18b
Carson Scott <carsonjscott14@gmail.com>
2017-08-18 20:57:43 -0400

    Added spatial learning

--- src/Network.py
+++ src/Network.py
@@ -11,14 +11,21 @@ class Network:
 		self.states = [0  for i in range(n)]
 		self.thresh = [0  for i in range(n)]
 		self.inputs = [[] for i in range(n)]
+		
+		self.nDelta = [0  for i in range(n)]
+		self.nAccel = [0  for i in range(n)]
+		self.nError = [0  for i in range(n)]
+
 		self.links = []
 		self.delay = []
 		self.pairs = []
 		self.times = []
-		self.delta = []
-		self.error = []
 		self.ready = []
-		self.accel = []
+
+		self.lDelta = []
+		self.lAccel = []
+		self.lError = []
+		
 		self.x = []
 		self.y = []
 		self.init()
@@ -30,12 +37,12 @@ class Network:
 	def connect(self, i, f):		
 		self.links.append(0)
 		self.delay.append(0)
-		self.error.append(0)
-		self.delta.append(0)
+		self.lError.append(0)
+		self.lDelta.append(0)
+		self.lAccel.append(0)
 		self.ready.append(0)
-		self.accel.append(0)
 
-		l = len(self.links)
+		l = len(self.links)-1
 		self.inputs[f].append(l)
 		self.pairs.append([i, f])
 		self.times.append([0, 0])
@@ -49,15 +56,14 @@ class Network:
 	def update_links(self, t):
 		
 		for r in range(len(self.pairs)):
-			DR = self.decay_rate
-
 			if self.links[r]:
 				self.links[r] = 0
-				self.error[r] = 0
-				self.delta[r] = 0
-			if self.accel[r] > 0: 
-				da = sigmoid(self.accel[r])*self.decay_rate
-				self.accel[r] -= da
+				self.lError[r] = 0
+				self.lDelta[r] = 0
+
+			if self.lAccel[r] > 0: 
+				da = sigmoid(self.lAccel[r])*self.decay_rate
+				self.lAccel[r] -= da
 			i = self.pairs[r][0]
 			f = self.pairs[r][1]
 
@@ -78,17 +84,60 @@ class Network:
 			elif self.ready[r]:
 				dp = dt-pt
 				pe = 0.5*dp**2
-				self.delta[r] = dp				
-				self.error[r] = pe 
+				self.lDelta[r] = dp				
+				self.lError[r] = pe 
 
-				LR = self.learn_rate
-				
-				da = self.delta[r]
-				dd = self.accel[r] + da
-				a = self.accel[r] + sigmoid(da)*LR
-				d = self.delay[r] + dd*LR
+				da = self.lDelta[r]
+				dd = self.lAccel[r] + da
+				a = self.lAccel[r] + sigmoid(da)*self.learn_rate
+				d = self.delay[r] + dd*self.learn_rate
 
-				self.accel[r] = a
+				self.lAccel[r] = a
 				self.delay[r] = d
 				self.links[r] = 1				
 				self.ready[r] = 0
+
+	def update_nodes(self):
+
+		for n in range(len(self.inputs)):
+			if self.states[n]: 
+				self.states[n] = 0
+				self.nError[n] = 0
+				self.nDelta[n] = 0
+
+			if self.nAccel[n] > 0:
+				dt = sigmoid(self.nAccel[n])*self.decay_rate
+				self.thresh[n] -= dt
+
+			xs = 0
+			xt = self.thresh[n]
+			
+			for r in range(len(self.inputs[n])):
+				xs += self.links[self.inputs[n][r]]
+
+			dp = xs-xt
+			if dp > 0:
+				pe = 0.5*dp**2
+				self.nDelta[n] = dp
+				self.nError[n] = pe
+
+				da = self.nDelta[n]
+				dt = self.nAccel[n] + da
+				a = self.nAccel[n] + sigmoid(da)*self.learn_rate
+				t = self.thresh[n] + dt*self.learn_rate
+
+				self.nAccel[n] = a
+				self.thresh[n] = t
+				self.states[n] = 1
+
+	def update(self, x, t):
+		for n in range(len(self.x)):
+			self.states[self.x[n]] = x[n]
+
+		self.update_links(t)
+		self.update_nodes()
+
+		y = []
+		for n in range(len(self.y)):
+			y.append(self.states[self.y[n]])
+		return y
